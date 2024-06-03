449e7a5e0611138c5983396ed301fcfdc099a51a
Jen Brissman <brissman514@gmail.com>
2024-06-02 23:53:28 -0700

    creating eager transformations

--- transformations/eagerAsync.js
+++ transformations/eagerAsync.js
@@ -3,3 +3,17 @@ const cloudinary = require('cloudinary').v2;
 cloudinary.config({ secure: "true" });
 
 
+cloudinary.uploader.upload(
+    "assets/plates.mp4",
+    {
+      resource_type: "video",
+      public_id: "plates",
+      eager: [
+        { width: 400, height: 300, crop: "pad" },
+        { width: 260, height: 200, crop: "crop", gravity: "north" }
+      ]
+    },
+    function(error, result) {
+      console.log(result, error);
+    }
+  );
