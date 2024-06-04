a0e789bef58181107435fa881607f29a947df4f4
Jen Brissman <brissman514@gmail.com>
2024-06-03 21:04:44 -0700

    cleaning up code

--- transformations/accelerate.js
+++ transformations/accelerate.js
@@ -4,7 +4,5 @@ const cloudinary = require('cloudinary').v2;
 // With accelerate, you can enhance user experience by speeding up video playback 
 // without compromising quality.
 
-
-
 const url = cloudinary.url("tuna", {effect: "accelerate:100", resource_type: 'video', urlAnalytics: false})
 console.log(url)
