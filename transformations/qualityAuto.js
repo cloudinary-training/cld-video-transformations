790a8873b59d6194fa500a0d304bedd61c4c426e
Jen Brissman <brissman514@gmail.com>
2024-06-03 20:29:10 -0700

    updating code for quality

--- transformations/qualityAuto.js
+++ transformations/qualityAuto.js
@@ -5,10 +5,5 @@ cloudinary.config({ secure: "true" });
 // The Quality Auto parameter is designed to automatically adjust the compression levels
 // of images and videos to achieve an optimal balance between visual quality and file size.
 
-
-const url = cloudinary.video('chef',
-    transformation = [
-      {quality: "auto"}
-    ])
-
-  console.log(url)
+const url = cloudinary.video("chef", {quality: "auto"})
+console.log(url)
